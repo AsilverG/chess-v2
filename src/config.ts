e89a30fc8406788d455ea7ae57d4bd13bc907301
Hyunseo Park <146466875+AsilverG@users.noreply.github.com>
2024-09-12 21:03:43 -0400

    Add files via upload

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA3ta10R9eMTQOKuxT4NwGPUZb7R4KsCu0",
+  authDomain: "chess-v2-db8b4.firebaseapp.com",
+  projectId: "chess-v2-db8b4",
+  storageBucket: "chess-v2-db8b4.appspot.com",
+  messagingSenderId: "759799093092",
+  appId: "1:759799093092:web:8fd5e63c8fe0edde520c71",
+  measurementId: "G-KKE58V3ML4"
+};
+
+export { firebaseConfig };
